e57a50ec7de6abeaadce760e0d086dbee4c54ca9
Raul Luzetti <raul.chiodi@gmail.com>
2022-01-19 15:43:48 -0300

    update(test)

--- test/addWords.js
+++ test/addWords.js
@@ -1,28 +1,48 @@
-require('assert');
-var Filter = require('../lib/badwords.js'),
+require("assert");
+var Filter = require("../lib/badwords.js"),
   filter = new Filter(),
-  assert = require('better-assert');
+  assert = require("better-assert");
 
-describe('filter', function(){
-  describe('addWords',function(){
-    it('Should append words to the filter list.', function(){
-      filter.addWords('dog', 'go');
-      assert(filter.clean('Go dog go') === '** *** **');
+describe("filter", function () {
+  describe("addWords", function () {
+    it("Should append words to the filter list.", function () {
+      filter.addWords("dog", "go");
+      assert(filter.clean("Go dog go") === "** *** **");
     });
 
-    it('Should append words to the filter using an array', () => {
-      let addWords = ['go', 'dog'];
-      filter = new Filter()
+    it("Should append words to the filter list with special characters.", function () {
+      filter.addWords("dóg", "gó");
+      assert(filter.clean("Go dog go") === "** *** **");
+    });
+
+    it("Should append words to the filter using an array", () => {
+      let addWords = ["go", "dog"];
+      filter = new Filter();
       filter.addWords(...addWords);
-      assert(filter.clean('Go dog go') === '** *** **');
+      assert(filter.clean("Go dog go") === "** *** **");
     });
 
-    it('Should allow a list to be passed to the constructor', function() {
+    it("Should append words to the filter using an array with special characters", () => {
+      let addWords = ["gó", "dóg"];
+      filter = new Filter();
+      filter.addWords(...addWords);
+      assert(filter.clean("Go dog go") === "** *** **");
+    });
+
+    it("Should allow a list to be passed to the constructor", function () {
       filter = new Filter({
-        list: ['dog']
+        list: ["dog"],
       });
 
-      assert(filter.clean('Go dog go') === 'Go *** go');
+      assert(filter.clean("Go dog go") === "Go *** go");
+    });
+
+    it("Should allow a list to be passed to the constructor with special characters", function () {
+      filter = new Filter({
+        list: ["dóg"],
+      });
+
+      assert(filter.clean("Go dog go") === "Go *** go");
     });
   });
 });
--- test/isProfane.js
+++ test/isProfane.js
@@ -1,44 +1,53 @@
-require('assert');
-var Filter = require('../lib/badwords.js'),
-	filter = new Filter(),
-	assert = require('better-assert');
+require("assert");
+var Filter = require("../lib/badwords.js"),
+  filter = new Filter(),
+  assert = require("better-assert");
 
-describe('filter', function(){
-	describe('isProfane',function(){
-		it("Should detect a bad word and return a boolean value",function(){
-			assert(filter.isProfane("ash0le"));
-		});
+describe("filter", function () {
+  describe("isProfane", function () {
+    it("Should detect a bad word and return a boolean value", function () {
+      assert(filter.isProfane("ash0le"));
+    });
 
-		it("Should return false when no bad word is detected",function(){
-			assert(filter.isProfane("wife") === false);
-		});
+    it("Should detect a bad word and return a boolean value escaping special characters", function () {
+      assert(filter.isProfane("ásh0le"));
+    });
 
-		it("Should be able to detect a bad word in a sentence",function(){
-			assert(filter.isProfane("that person is an ash0le"));
-		});
+    it("Should return false when no bad word is detected", function () {
+      assert(filter.isProfane("wife") === false);
+    });
 
-		it('Filters out special characters appropriately', function() {
-			assert(filter.isProfane("You're an asshole^ you are"));
-		});
+    it("Should be able to detect a bad word in a sentence", function () {
+      assert(filter.isProfane("that person is an ash0le"));
+    });
 
-		it('Should detect filtered words from badwords-list', function(){
-			assert(filter.isProfane('willies'));
-		});
+    it("Filters out special characters appropriately", function () {
+      assert(filter.isProfane("You're an asshole^ you are"));
+    });
 
-		it('Should detect filtered words regardless of type case', function() {
-			var filter = new Filter({
-				list: ['Test']
-			});
-			assert(filter.isProfane('test'));
-		});
+    it("Should detect filtered words from badwords-list", function () {
+      assert(filter.isProfane("willies"));
+    });
 
-		it('Should tokenize words according to regex word boundaries', function() {
-			assert(filter.isProfane("that person is an\nasshole"));
-		});
+    it("Should detect filtered words regardless of type case", function () {
+      var filter = new Filter({
+        list: ["Test"],
+      });
+      assert(filter.isProfane("test"));
+    });
 
-		it('Should detect bad word phrases', function () {
-			filter.addWords('oh no');
-			assert(filter.isProfane("oh no! this is profane!"));
-		});
-	});
+    it("Should tokenize words according to regex word boundaries", function () {
+      assert(filter.isProfane("that person is an\nasshole"));
+    });
+
+    it("Should detect bad word phrases", function () {
+      filter.addWords("oh no");
+      assert(filter.isProfane("oh no! this is profane!"));
+    });
+
+    it("Should detect bad word phrases with special characters", function () {
+      filter.addWords("óh nó");
+      assert(filter.isProfane("óh nó this is profane!"));
+    });
+  });
 });
